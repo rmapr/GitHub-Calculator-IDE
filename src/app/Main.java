84c0586ff0152a5282aff49644aaa2ee8305a196
rmapr <rmap@ukr.net>
2023-04-02 15:42:50 +0300

    Add division

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,6 +13,8 @@ public class Main {
         System.out.println(d);
         int resultSubtraction = subtraction(a, b);
         System.out.printf("The result of subtracting numbers %d and %d is %d", a, b, resultSubtraction);
+        int resultDivision = division(a, b);
+        System.out.printf("%nThe result of dividing numbers %d and %d is %d", a, b, resultDivision);
     }
 
     private static int add(int a, int b) {
@@ -25,5 +27,8 @@ public class Main {
 
     private static int subtraction(int a, int b) {
         return a - b;
+    }
+    private static int division(int a, int b) {
+        return a / b;
     }
 }
