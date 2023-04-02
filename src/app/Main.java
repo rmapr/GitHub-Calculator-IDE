a161571b63527b32ffa86ae0458750b1313f3e61
rmapr <rmap@ukr.net>
2023-04-02 15:44:26 +0300

    Declaration of Variables

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,18 +2,23 @@ package app;
 
 public class Main {
 
+    static int c;
+    static int d;
+    static int resultSubtraction;
+    static int resultDivision;
+
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
         int a = 8;
         int b = 2;
-        int c = add(a, b);
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int resultSubtraction = subtraction(a, b);
+        resultSubtraction = subtraction(a, b);
         System.out.printf("The result of subtracting numbers %d and %d is %d", a, b, resultSubtraction);
-        int resultDivision = division(a, b);
+        resultDivision = division(a, b);
         System.out.printf("%nThe result of dividing numbers %d and %d is %d", a, b, resultDivision);
     }
 
